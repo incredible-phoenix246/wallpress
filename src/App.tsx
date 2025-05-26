c56808a469ce8ddce096a86e48c408cae04f6d95
phoenix <ayomikuntemitope.araoye@gmail.com>
2025-05-26 16:56:35 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
-const App = () => {
-  return <div>App</div>;
-};
+import CommentPopup from "./components/miscellaneous/comment-popup";
+import SmoothCursor from "./components/miscellaneous/cursor";
+import CTASection from "./components/sections/cta-section";
+import FAQSection from "./components/sections/faq-section";
+import FeaturesSection from "./components/sections/features-section";
+import Hero from "./components/sections/herosection";
+import HowItWorks from "./components/sections/how-it-works";
+import { UseCaseSection } from "./components/sections/use-case";
+import Navbar from "./components/navigation/navbar";
+import Footer from "./components/navigation/footer";
 
-export default App;
+export default function Home() {
+  return (
+    <>
+      <main className="bg-[#F6F5F0]">
+        <Navbar />
+        <Hero />
+        <FeaturesSection />
+        <HowItWorks />
+        <UseCaseSection />
+        <FAQSection />
+        <CTASection />
+
+        <SmoothCursor />
+        <Footer />
+        <CommentPopup />
+      </main>
+    </>
+  );
+}
